4c05198015a3c465d83da497ea5a14a51f08db68
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-03 14:07:22 +0530

    Create Order class

---
+++ src/main/java/lk/ijse/dep11/pos/tm/Item.java
@@ -0,0 +1,24 @@
+package lk.ijse.dep11.pos.tm;
+
+import com.sun.source.doctree.SerialDataTree;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item implements Serializable {
+    private String code;
+    private String description;
+    private int quantity;
+    private BigDecimal unitPrice;
+
+    @Override
+    public String toString() {
+        return code;
+    }
+}
