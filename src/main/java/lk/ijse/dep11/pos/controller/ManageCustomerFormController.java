937ccb1281747911351252b58bee750ad1006d3d
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-04 10:22:47 +0530

    Add Customer Data Access Class and Intialize method in Manage Customer Form Controller

--- src/main/java/lk/ijse/dep11/pos/controller/ManageCustomerFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/ManageCustomerFormController.java
@@ -7,13 +7,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.dep11.pos.db.CustomerDataAccess;
+import lk.ijse.dep11.pos.tm.Customer;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 
 public class ManageCustomerFormController {
     public AnchorPane root;
@@ -22,12 +27,47 @@ public class ManageCustomerFormController {
     public JFXTextField txtCustomerAddress;
     public JFXButton btnSave;
     public JFXButton btnDelete;
-    public TableView tblCustomers;
+    public TableView<Customer> tblCustomers;
+    public JFXButton btnAddNew;
+
 
     //********************** Navigation to Home ********************************************
     public void navigateToHome(MouseEvent mouseEvent) throws IOException {
         MainFormController.navigateToMain(root);
     }
+
+    //********************** Initialize Method ********************************************
+    public void initialize() {
+        tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblCustomers.getColumns().get(0).setStyle("-fx-alignment: center;");
+        tblCustomers.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblCustomers.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
+        txtCustomerId.setEditable(false);
+        btnDelete.setDisable(true);
+        btnSave.setDefaultButton(true);
+        btnAddNew.fire();
+        try {
+            tblCustomers.getItems().addAll(CustomerDataAccess.getAllCustomers());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to load customers, try later!").show();
+        }
+        tblCustomers.getSelectionModel().selectedItemProperty().addListener((ov, prev, cur) ->{
+            if (cur != null){
+                btnSave.setText("UPDATE");
+                btnDelete.setDisable(false);
+                txtCustomerId.setText(cur.getId());
+                txtCustomerName.setText(cur.getName());
+                txtCustomerAddress.setText(cur.getAddress());
+            }else{
+                btnSave.setText("SAVE");
+                btnDelete.setDisable(true);
+            }
+        });
+        Platform.runLater(txtCustomerName::requestFocus);
+    }
+
+
     //********************** Add New Customer Button ****************************************
     public void btnAddNew_OnAction(ActionEvent actionEvent) {
     }
